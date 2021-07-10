533b38f250a1bfc80ca414ed30e83442027df270
Guilherme Medeiros <guilherme.medeiros@southsystem.com.br>
2021-07-10 13:29:28 -0300

    Criando serialização

--- src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -3,6 +3,7 @@ package br.com.alura.ecommerce;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.math.BigDecimal;
 import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
@@ -10,20 +11,25 @@ import java.util.concurrent.ExecutionException;
 public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try (var dispatcher = new KafkaDispatcher()) {
+        var userId = UUID.randomUUID().toString();
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
             for (int i = 0; i < 10; i++) {
-                var key = UUID.randomUUID().toString();
 
-                var value = "132123,67523,1234";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                var orderId = UUID.randomUUID().toString();
+                var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
+                var order = new Order(userId, orderId, amount);
+                orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-                var email = "Thank you for your order! We are processing your order!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
             }
         }
 
-
-    }
+        try (var emailDispatcher = new KafkaDispatcher<String>()) {
+            for (int i = 0; i < 10; i++) {
+                var email = "Thank you for your order! We are processing your order!";
+                emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+            }
+        }
+     }
 
     private static Properties properties() {
         var properties = new Properties();
